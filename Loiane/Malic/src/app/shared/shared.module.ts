69ad180a0c240679aa9c640997fe3146a6544b1f
Enzo Glauber de Lima Almeida <enzojob@gmail.com>
2017-08-02 10:57:27 -0300

    Shared Module

--- Loiane/Malic/src/app/shared/shared.module.ts
+++ Loiane/Malic/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { FundoAmareloDirective } from './fundo-amarelo.directive';
 import { HighlightMouseDirective } from './highlight-mouse.directive';
@@ -9,12 +10,13 @@ import { HighlightDirective } from './highlight.directive';
 import { CamelCasePipe } from '../shared/camel-case.pipe';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, RouterModule ],
+  imports: [ CommonModule, FormsModule, RouterModule, HttpModule ],
   declarations: [ CamelCasePipe, FundoAmareloDirective, HighlightMouseDirective, HighlightDirective],
   exports: [
     CommonModule,
     FormsModule,
     RouterModule,
+    HttpModule,
     CamelCasePipe,
     FundoAmareloDirective,
     HighlightMouseDirective,
